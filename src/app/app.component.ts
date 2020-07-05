2f7de5595b62167fd1450a18c00d9919e03fc9bf
LiciaLegacy <licialegacy@gmail.com>
2020-07-05 15:36:47 -0400

    added initial naviation with event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  loadedFeature = 'recipe';
+
+  //this 'feature' is the feature we want to navigate to
+  //and now above we create a property to actually store this feature
+  //and here we set this property to what we receive as an argument
+  //and thn now on the app component .html page, we use ngIf to determine which of the 2 will be displayed
+  onNavigate(feature:string){
+    this.loadedFeature = feature;
+  }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent{
+    //we have to attach something to this to make it listenable from the parent component
+    @Output() featureSelected = new EventEmitter<string>(); //we pass string, the feature which was selected, and we have to instantiate this; 
+    //call this to create an object based on the EventEmitter class
+    //we will then be passing this, or firing the event to the app component; the app html component has the (featureSelected) event handler, 
+    //by which it triggers onNavigate($event) to the app component 
+    //where we will receive the 'where should we navigate to' information
 
+    //we created this method first, then created the above event emitter, 'featureSelected'
+    onSelect(feature:string){
+        //and here we want to emit our own event, signaling which header/navbar feature has been clicked
+        this.featureSelected.emit(feature);
+    }
 }
 
